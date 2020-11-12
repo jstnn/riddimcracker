2dc45ef068f40a2ac2d42a4016d31493d2687570
Nicolas Justiniano <njustini@thoughtworks.com>
2020-11-12 15:52:05 -0300

    [@jstnn] añadiendo hook is-sound y un drum machine simple

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import DrumMachine from '../src/components/DrumMachine';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Riddimcracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Riddimcracker v0.1
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <DrumMachine />
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
